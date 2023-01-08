15fd22e081b9491885cfb20795698b2ef4220e07
leo <fmleonardo@protonmail.com>
2023-01-07 23:03:01 -0300

    more stuff yay :D

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env sh
+pkill polybar
 
-# Terminate already running bar instances
-killall -q polybar
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar
-#polybar top --config=~/.config/polybar/config.ini &
-polybar main --config=~/.config/polybar/config.ini
+if type "xrandr"; then
+	for m in $(xrandr --query | grep 'connected' | cut -d" " -f1); do
+		MONITOR=$m polybar --reload main --config=~/.config/polybar/config.ini &
+	done
+else
+	polybar --reload main --config=~/.config/polybar/config.ini &
+fi
